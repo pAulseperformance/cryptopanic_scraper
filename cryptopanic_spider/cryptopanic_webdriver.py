24a64fe02a9da82dd70d890819eef467e5c19b5b
grilledchickenthighs <pauldmendes5@gmail.com>
2019-02-03 14:54:01 -0500

    Working version 0.01

--- cryptopanic_spider/cryptopanic_webdriver.py
+++ cryptopanic_spider/cryptopanic_webdriver.py
@@ -17,7 +17,7 @@ def setUp():
     options = webdriver.ChromeOptions()
 
     # initialize headless mode
-    # options.add_argument('headless')
+    options.add_argument('headless')
     # webdriver.ChromeOptions().extensions
     # webdriver.ChromeOptions().add_extension()
 
@@ -40,96 +40,103 @@ def getData():
 
     elements = driver.find_elements_by_css_selector('div.news-row.news-row-link')
     print(len(elements))
+    try:
+        for i in range(len(elements)):
+            print(i)
+            print(len(elements))
 
-    # count = 0
-    for i in range(len(elements)):
-        print(i)
-        print(len(elements))
+            #  Get date posted
+            date_time = elements[i].find_element_by_css_selector('time').get_attribute('datetime')
 
-        #  Debug
-        # if count == 2:
-        #     count = 0
-        #     break
-        elements[i].find_element_by_css_selector('time')
-        #  Get date posted
-        date_time = elements[i].find_element_by_css_selector('time').get_attribute('datetime')
-        # print(date_time)
-
-        #  Get Title of News
-        # title = element.find_element_by_class_name("title-text").text
-        title = elements[i].find_element_by_css_selector("span.title-text").text
-        if title == '':
-            print("Empty title below")
-            print(title)
-            print("Scrolling")
-            scrollCenter()
+            #  Get Title of News
             title = elements[i].find_element_by_css_selector("span.title-text").text
-            print("Not empty")
-            print(title)
+            if title == '':
+                driver.execute_script("arguments[0].scrollIntoView();",
+                                      elements[i].find_element_by_css_selector("span.title-text"))
 
+                # print("Empty title below")
+                # print(title)
+                # print("Scrolling")
+                # # scrollCenter()
+                title = elements[i].find_element_by_css_selector("span.title-text").text
+                # print("Not empty")
+                # print(title)
 
-        # TODO Get Link Source
+            # TODO Get Link Source
 
-        #  Get Currency Tags
-        currency_elements = elements[i].find_elements_by_class_name("colored-link")
-        currencies = []
-        for currency in currency_elements:
-            currencies.append(currency.text)
+            #  Get Currency Tags
+            currency_elements = elements[i].find_elements_by_class_name("colored-link")
+            currencies = []
+            for currency in currency_elements:
+                currencies.append(currency.text)
 
-        nc_votes = elements[i].find_elements_by_css_selector("span.nc-vote-cont")
-        votes = []
-        for nc_vote in nc_votes:
-            votes.append(nc_vote.get_attribute('title'))
-        try:
-            data[i] = {"Date": date_time,
-                       "Title": title,
-                       "Currencies": currencies,
-                       "Votes": votes}
-        except Exception as e:
-            print(e)
-            print("Element index is %s" % i)
-            print("Length of elements is %s" % len(elements))
-            break
-
-        # count += 1
-
+            nc_votes = elements[i].find_elements_by_css_selector("span.nc-vote-cont")
+            votes = []
+            for nc_vote in nc_votes:
+                votes.append(nc_vote.get_attribute('title'))
+            try:
+                data[i] = {"Date": date_time,
+                           "Title": title,
+                           "Currencies": currencies,
+                           "Votes": votes}
+            except Exception as e:
+                print(e)
+                print("Element index is %s" % i)
+                print("Length of elements is %s" % len(elements))
+                break
+    except Exception as e:
+        print(e)
     return len(elements), data
 
 
-def loadMore():
+def loadMore(len_elements):
     # Load More News
+
     load_more = driver.find_element_by_class_name('btn-outline-primary')
+
     driver.execute_script("arguments[0].scrollIntoView();", load_more)
+    time.sleep(1)
 
+    elements = driver.find_elements_by_css_selector('div.news-row.news-row-link')
+    print(len_elements, len(elements))
 
-def scrollCenter():
-    #  Scrolls to Center
-    scroll = driver.find_element_by_class_name('ps__rail-y')
-    scroll.click()
-    # driver.get_window_position()
-    # scroll.get_attribute('offsetTop')
-    # scroll.get_attribute('style')
+    if len_elements < len(elements):
+        print("loaded %s more rows" % (len(elements) - len_elements))
+        return True
+    else:
+        print("No more rows to load :/")
+        print("Max rows loaded: %s" % len(elements))
+        return False
 
 
 def tearDown():
     driver.quit()
 
-
 driver = setUp()
 
 import time
 SCROLL_PAUSE_TIME = 1
-count = 0
-while count < 10:
-    print(count)
-    lenData, data = getData()
-    loadMore()
-    print(lenData, data)
-    count += 1
-    time.sleep(SCROLL_PAUSE_TIME)
 
-print("finished!")
-print(lenData, data)
+
+while True:
+    elements = driver.find_elements_by_css_selector('div.news-row.news-row-link')
+    print("Total Rows loaded:%s" % len(elements))
+    # time.sleep(SCROLL_PAUSE_TIME)
+    if loadMore(len(elements)):
+        continue
+    else:
+        print("Gathering Data...")
+        try:
+            len_data, data = getData()
+        except:
+            print(len_data, data)
+            print("finished!")
+        tearDown()
+        break
 tearDown()
-
-getData()
+print(len_data)
+print(len(elements))
+# Save the website data
+import pickle
+with open('cryptocompare.pickle', 'wb') as f:
+          pickle.dump(data, f)
