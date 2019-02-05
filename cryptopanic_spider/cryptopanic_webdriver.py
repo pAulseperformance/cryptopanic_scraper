3e6871c814b61dd8b85d76e9e8d62a3473d7365d
grilledchickenthighs <pauldmendes5@gmail.com>
2019-02-04 22:13:28 -0500

    added persistence pickle and date formatting

--- cryptopanic_spider/cryptopanic_webdriver.py
+++ cryptopanic_spider/cryptopanic_webdriver.py
@@ -3,25 +3,28 @@ import config
 import os
 import time
 import datetime
+import sys
+import re
+import pickle
+
+
 SCROLL_PAUSE_TIME = 1
 
 
-def getProperties():
-    properties = driver.execute_script('return window.getComputedStyle(arguments[0], null);', element)
-
-    for property in properties:
-        print(element.value_of_css_property(property))
-
-
 def setUp():
+    try:
+        filter = sys.argv[1]
+    except Exception as e:
+        print(e)
+        filter = 'All'
 
-    url = "https://www.cryptopanic.com"
+    url = "https://www.cryptopanic.com/news?filter={}".format(filter)
     chromedriver_path = os.path.join(config.ROOT_DIR, "chromedriver")
 
     options = webdriver.ChromeOptions()
 
     # initialize headless mode
-    options.add_argument('headless')
+    # options.add_argument('headless')
     # webdriver.ChromeOptions().extensions
     # webdriver.ChromeOptions().add_extension()
 
@@ -39,49 +42,6 @@ def setUp():
     return driver
 
 
-# def getData():
-#
-#     data = dict()
-#     elements = driver.find_elements_by_css_selector('div.news-row.news-row-link')
-#     try:
-#         for i in range(len(elements)):
-#
-#             #  Get date posted
-#             date_time = elements[i].find_element_by_css_selector('time').get_attribute('datetime')
-#
-#             #  Get Title of News
-#             title = elements[i].find_element_by_css_selector("span.title-text").text
-#             if title == '':
-#                 driver.execute_script("arguments[0].scrollIntoView();",
-#                                       elements[i].find_element_by_css_selector("span.title-text"))
-#                 title = elements[i].find_element_by_css_selector("span.title-text").text
-#
-#             # TODO Get Link Source
-#
-#             #  Get Currency Tags
-#             currencies = []
-#             currency_elements = elements[i].find_elements_by_class_name("colored-link")
-#             for currency in currency_elements:
-#                 currencies.append(currency.text)
-#
-#             votes = []
-#             nc_votes = elements[i].find_elements_by_css_selector("span.nc-vote-cont")
-#             for nc_vote in nc_votes:
-#                 votes.append(nc_vote.get_attribute('title'))
-#             try:
-#                 data[i] = {"Date": date_time,
-#                            "Title": title,
-#                            "Currencies": currencies,
-#                            "Votes": votes}
-#             except Exception as e:
-#                 print(e)
-#                 print("Element index is %s" % i)
-#                 print("Length of elements is %s" % len(elements))
-#                 break
-#     except Exception as e:
-#         print(e)
-#     return len(elements), data
-
 def getData():
 
     data = dict()
@@ -91,6 +51,8 @@ def getData():
 
         #  Get date posted
         date_time = elements[i].find_element_by_css_selector('time').get_attribute('datetime')
+        string_date = re.sub('-.*', '', date_time)
+        date_time = datetime.datetime.strptime(string_date, "%a %b %d %Y %H:%M:%S %Z")
 
         #  Get Title of News
         title = elements[i].find_element_by_css_selector("span.title-text").text
@@ -127,6 +89,7 @@ def getData():
 
 def loadMore(len_elements):
     # Load More News
+    return False
     load_more = driver.find_element_by_class_name('btn-outline-primary')
     driver.execute_script("arguments[0].scrollIntoView();", load_more)
 
@@ -144,10 +107,9 @@ def loadMore(len_elements):
 
 def tearDown():
     driver.quit()
-tearDown()
+
 
 driver = setUp()
-
 
 while True:
     elements = driver.find_elements_by_css_selector('div.news-row.news-row-link')
@@ -163,10 +125,20 @@ while True:
         tearDown()
         break
 
+
 print(data)
 # Save the website data
-import pickle
-file_name = "cryptopanic_all_%s.pickle" % datetime.datetime.utcnow().date()
-file_name
+filter = 'All'
+
+file_name = "cryptopanic_{}_{}->{}.pickle".format(filter.lower(),
+                                                  data[len(data)-1]['Date'].__str__(),
+                                                  data[0]['Date'].__str__())
+
+
 with open(file_name, 'wb') as f:
           pickle.dump(data, f)
+
+# with open('cryptopanic_all_2019-02-03.pickle', 'rb') as f:
+#     data = pickle.load(f)
+# line = re.sub('-.*', '', str(data[len(data)-1]['Date']))
+# line
