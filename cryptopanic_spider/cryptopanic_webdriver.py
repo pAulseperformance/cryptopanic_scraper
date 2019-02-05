defb57c51abf088a2779cf9e8b8ecc2e4d251160
grilledchickenthighs <pauldmendes5@gmail.com>
2019-02-05 00:36:41 -0500

    Fixed some wierd issue with selenium loading up the first 7 rows twice

--- cryptopanic_spider/cryptopanic_webdriver.py
+++ cryptopanic_spider/cryptopanic_webdriver.py
@@ -32,8 +32,10 @@ def setUp():
     options.add_argument('window-size=1200x800')
 
     # initialize the driver
+    print("Initializing chromedriver.\n")
     driver = webdriver.Chrome(chrome_options=options, executable_path=chromedriver_path)
 
+    print("Navigating to %s\n" % url)
     driver.get(url)
 
     # wait up to 10 seconds for the elements to become available
@@ -42,14 +44,33 @@ def setUp():
     return driver, filter
 
 
+def loadMore(len_elements):
+    # Load More News
+    load_more = driver.find_element_by_class_name('btn-outline-primary')
+    driver.execute_script("arguments[0].scrollIntoView();", load_more)
+
+    time.sleep(SCROLL_PAUSE_TIME)
+
+    elements = driver.find_elements_by_css_selector('div.news-row.news-row-link')
+    if len_elements < len(elements):
+        print("Loading %s more rows" % (len(elements) - len_elements))
+        return True
+    else:
+        print("No more rows to load :/")
+        print("Total rows loaded: %s\n" % len(elements))
+        return False
+
+
 def getData():
     data = dict()
     elements = driver.find_elements_by_css_selector('div.news-row.news-row-link')
 
-    print("Gathering Data...")
+    total_rows = len(elements) - 7  # elements being returned are appended by 7 of the first rows.
+    print("Downloading Data...\n")
     start = datetime.datetime.today()
-    print("Time Start: %s" % start.ctime())
-    for i in range(len(elements)):
+    print("Time Start: %s\n" % start.ctime())
+    for i in range(total_rows):
+        # print("Downloading Row %s of %s" % (i + 1, total_rows))
 
         #  Get date posted
         date_time = elements[i].find_element_by_css_selector('time').get_attribute('datetime')
@@ -75,64 +96,66 @@ def getData():
         nc_votes = elements[i].find_elements_by_css_selector("span.nc-vote-cont")
         for nc_vote in nc_votes:
             votes.append(nc_vote.get_attribute('title'))
-        try:
-            data[i] = {"Date": date_time,
-                       "Title": title,
-                       "Currencies": currencies,
-                       "Votes": votes}
-        except Exception as e:
-            print(e)
-            print("Element index is %s" % i)
-            print("Length of elements is %s" % len(elements))
-            break
 
-    print("Finished gathering %s rows of data" % len(elements))
+        data[i] = {"Date": date_time,
+                   "Title": title,
+                   "Currencies": currencies,
+                   "Votes": votes}
+        print("Downloaded %s of %s\nTitle: %s\nPublished: %s\n" % (i + 1,
+                                                                   total_rows,
+                                                                   data[i]['Title'],
+                                                                   data[i]["Date"]))
+
+    print("Finished gathering %s rows of data\n" % len(data))
     print("Time End: %.19s" % datetime.datetime.now())
-    print("Elapsed Time Gathering Data: %.7s" % (datetime.datetime.now() - start))
+    print("Elapsed Time Gathering Data: %.7s\n" % (datetime.datetime.now() - start))
 
     return data
 
 
-def loadMore(len_elements):
-    # Load More News
-    load_more = driver.find_element_by_class_name('btn-outline-primary')
-    driver.execute_script("arguments[0].scrollIntoView();", load_more)
-
-    time.sleep(SCROLL_PAUSE_TIME)
-
-    elements = driver.find_elements_by_css_selector('div.news-row.news-row-link')
-    if len_elements < len(elements):
-        print("Loading %s more rows" % (len(elements) - len_elements))
-        return True
-    else:
-        print("No more rows to load :/")
-        print("Max rows loaded: %s" % len(elements))
-        return False
-
-
 def saveData(data):
     # Save the website data
-    file_name = "cryptopanic_{}_{}->{}.pickle".format(filter.lower(),
-                                                      str(data[len(data) - 1]['Date']),
-                                                      str(data[0]['Date']))
-    print("Saving data to %s" % file_name)
+    file_name = "cryptopanic_{}_{:.10}->{:.10}.pickle".format(filter.lower(),
+                                                              str(data[len(data) - 1]['Date']),
+                                                              str(data[0]['Date']))
+    print("Saving data to %s\n" % file_name)
     with open(file_name, 'wb') as f:
         pickle.dump(data, f)
 
 
 def tearDown():
+    print("Exiting Headless Chrome Driver\n")
     driver.quit()
 
 
 driver, filter = setUp()
 
+print("Scrolling and Loading News Feed. ;)")
 while True:
+
     elements = driver.find_elements_by_css_selector('div.news-row.news-row-link')
+
     if loadMore(len(elements)):
         continue
     else:
-        print("Total Rows loaded:%s" % len(elements))
         data = getData()
         saveData(data)
         tearDown()
         break
+
+
+def loadData():
+
+    # Save the website data
+    file_name = "cryptopanic_hot_2019-02-03 11:10:04->2019-02-04 15:00:46.pickle"
+    with open(file_name, 'rb') as f:
+        return pickle.load(f)
+# data = loadData()
+# print(data[len(data)-8]['Date'])
+# print(data[2]['Date'])
+# file_name = "cryptopanic_{}_{:.10}->{:.10}.pickle".format(filter.lower(),
+#                                                   str(data[len(data) - 8]['Date']),
+#                                                   str(data[0]['Date']))
+# file_name
+# for i in range(len(data)):
+#     print(i, data[i]["Date"])
